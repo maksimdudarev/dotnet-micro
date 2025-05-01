f25564db540ef4b783b98359adf3aaeb9ff98866
Maksim Dudarev <dudarev_mn@a-med.space>
2025-05-01 23:40:42 +0300

    add packages, models, db context, repo

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
