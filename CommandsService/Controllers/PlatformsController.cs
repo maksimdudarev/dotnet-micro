e4f878600ed12e21647b36c1668618e292dd7bb7
Maksim Dudarev <dudarev_mn@a-med.space>
2025-05-06 18:08:14 +0300

    add platforms prod json with command path

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -14,7 +14,7 @@ namespace CommandsService.Controllers
     [HttpPost]
     public ActionResult TestInboundConnection()
     {
-      Console.WriteLine("--> Inbound POST # Command Sevice");
+      Console.WriteLine("--> Inbound POST # Command Service");
 
       return Ok("Inbound test of from Platforms Controller");
     }
