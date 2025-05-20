5ad5d61a13032b5fe817bac9a1276e2892da2dd3
Maksim Dudarev <dudarev_mn@a-med.space>
2025-05-20 18:44:32 +0300

    determine event

---
+++ CommandsService/EventProcessing/EventProcessor.cs
@@ -0,0 +1,46 @@
+using System.Text.Json;
+using AutoMapper;
+using CommandsService.Dtos;
+
+namespace CommandsService.EventProcessing
+{
+  public class EventProcessor : IEventProcessor
+  {
+    private readonly IServiceScopeFactory _scopeFactory;
+    private readonly IMapper _mapper;
+
+    public EventProcessor(IServiceScopeFactory scopeFactory, IMapper mapper)
+    {
+      _scopeFactory = scopeFactory;
+      _mapper = mapper;
+    }
+
+    public void ProcessEvent(string message)
+    {
+      throw new NotImplementedException();
+    }
+
+    private EventType DetermineEvent(string notificationMessage)
+    {
+      Console.WriteLine("--> Determining Event");
+
+      var eventType = JsonSerializer.Deserialize<GenericEventDto>(notificationMessage);
+
+      switch (eventType.Event)
+      {
+        case "Platform_Published":
+          Console.WriteLine("--> Platform Published Event Detected");
+          return EventType.PlatformPublished;
+        default:
+          Console.WriteLine("--> Could not determine the event type");
+          return EventType.Undetermined;
+      }
+    }
+  }
+
+  enum EventType
+  {
+    PlatformPublished,
+    Undetermined
+  }
+}
