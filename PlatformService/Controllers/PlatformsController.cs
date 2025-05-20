443cc6a9ee82af09d5c6d7ae4bcbb904596a21b2
Maksim Dudarev <dudarev_mn@a-med.space>
2025-05-20 16:24:30 +0300

    add publish message to ctrl

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using PlatformService.AsyncDataServices;
 using PlatformService.Data;
 using PlatformService.Dtos;
 using PlatformService.Models;
@@ -14,12 +15,14 @@ namespace PlatformService.Controllers
     private readonly IPlatformRepo _repository;
     private readonly IMapper _mapper;
     private readonly ICommandDataClient _commandDataClient;
+    private readonly IMessageBusClient _messageBusClient;
 
-    public PlatformsController(IPlatformRepo repository, IMapper mapper, ICommandDataClient commandDataClient)
+    public PlatformsController(IPlatformRepo repository, IMapper mapper, ICommandDataClient commandDataClient, IMessageBusClient messageBusClient)
     {
       _repository = repository;
       _mapper = mapper;
       _commandDataClient = commandDataClient;
+      _messageBusClient = messageBusClient;
     }
 
     [HttpGet]
@@ -63,7 +66,18 @@ namespace PlatformService.Controllers
         Console.WriteLine($"Could not send synchronously: {ex.Message}");
       }
 
-      return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id}, platformReadDto);
+      try
+      {
+        var platformPublishedDto = _mapper.Map<PlatformPublishedDto>(platformReadDto);
+        platformPublishedDto.Event = "Platform_Published";
+        _messageBusClient.PublishNewPlatform(platformPublishedDto);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Could not send asynchronously: {ex.Message}");
+      }
+
+      return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
     }
   }
 }
