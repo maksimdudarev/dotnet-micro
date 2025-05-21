de653d76c15043076ce33deb5edf27979c0b559d
Maksim Dudarev <dudarev_mn@a-med.space>
2025-05-21 14:24:37 +0300

    message subscriber part 2 + register

--- CommandsService/AsyncDataServices/MessageBusSubscriber.cs
+++ CommandsService/AsyncDataServices/MessageBusSubscriber.cs
@@ -1,5 +1,7 @@
+using System.Text;
 using CommandsService.EventProcessing;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace CommandsService.AsyncDataServices
 {
@@ -41,7 +43,23 @@ namespace CommandsService.AsyncDataServices
 
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
-      throw new NotImplementedException();
+      stoppingToken.ThrowIfCancellationRequested();
+
+      var consumer = new EventingBasicConsumer(_channel);
+
+      consumer.Received += (ModuleHandle, ea) =>
+      {
+        Console.WriteLine("--> Event Received!");
+
+        var body = ea.Body;
+        var notificationMessage = Encoding.UTF8.GetString(body.ToArray());
+
+        _eventProcessor.ProcessEvent(notificationMessage);
+      };
+
+      _channel.BasicConsume(queue: _queueName, autoAck: true, consumer: consumer);
+
+      return Task.CompletedTask;
     }
 
     private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e)
--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,3 +1,4 @@
+using CommandsService.AsyncDataServices;
 using CommandsService.Data;
 using CommandsService.EventProcessing;
 using Microsoft.EntityFrameworkCore;
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<ICommandRepo, CommandRepo>();
+builder.Services.AddHostedService<MessageBusSubscriber>();
 builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
